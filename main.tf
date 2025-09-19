fd13aa73d68cf1e3f187e06c025549e66fcc957a
amvanmaren <vanmaren@gmail.com>
2025-09-18 17:17:36 -0700

    Added search and creation of Ubuntu EC2 instance

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
 provider "aws" {
-    region = "us-west-1"
+  region  = "us-west-1"
+  profile = "default"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+ filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "example_name" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "example_name"
+  }
+}
