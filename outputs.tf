f1ca8dac25c4e82837cd562ceee37ff9d520601b
amvanmaren <vanmaren@gmail.com>
2025-09-21 14:58:30 -0700

    Changed to smaller t3.micro. and ran terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "instancce_hostname" {
-    description = "Private DNS name of the EC2 instance."
-    value = aws_instance.example_name.private_dns
+  description = "Private DNS name of the EC2 instance."
+  value       = aws_instance.example_name.private_dns
 }
