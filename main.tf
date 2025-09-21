a4dc4af912ed3fc3d87219a2ad5ceceaacb3d0aa
amvanmaren <vanmaren@gmail.com>
2025-09-21 14:57:53 -0700

    Added module for AWS VPC, and moved the example EC2 instance inside private subnet.

--- main.tf
+++ main.tf
@@ -6,18 +6,36 @@ provider "aws" {
 data "aws_ami" "ubuntu" {
   most_recent = true
 
- filter {
+  filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-  
+
   owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "example_name" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
+
   tags = {
     Name = var.instance_name
   }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0.1"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-1a", "us-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames = true
 }
